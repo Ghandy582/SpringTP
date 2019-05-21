fa52205a6499b684613dd52a616755bec002c3ee
Shusii <36765198+Shusii@users.noreply.github.com>
2019-05-21 09:51:02 +0200

    'tgata'erg'zeghzrth(rzh'rthr

--- SpringTP/src/fr/cs2i/MainApp.java
+++ SpringTP/src/fr/cs2i/MainApp.java
@@ -5,7 +5,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
    public static void main(String[] args) {
+	   // créer un contexte d'application
       ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+      // récupérer le bean helloWorld
       HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
       obj.getMessage();
    }
