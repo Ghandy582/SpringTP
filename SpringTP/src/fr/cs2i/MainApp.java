b03820170e76cbb465ed06168952e505c43c24bc
kingc <kingc@DESKTOP-SAP6E8H>
2019-05-23 08:44:17 +0200

    Spring - Bean Life Cycle

--- SpringTP/src/fr/cs2i/MainApp.java
+++ SpringTP/src/fr/cs2i/MainApp.java
@@ -1,14 +1,23 @@
 package fr.cs2i;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
    public static void main(String[] args) {
-	   // créer un contexte d'application
-      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+	  // créer un contexte d'application, initialisation des beans
+	  AbstractApplicationContext  context = new ClassPathXmlApplicationContext("Beans.xml");
       // récupérer le bean helloWorld
-      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-      obj.getMessage();
+      HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+      //méthode 1
+      objA.setMessage("I'm object A");
+      objA.getMessage();
+
+      // récupérer le bean helloWorld
+      HelloWorld objB = (HelloWorld) context.getBean("helloWorld");
+      objB.getMessage();
+      // fermeture des beans initialisé
+      context.registerShutdownHook(); 
    }
 }
