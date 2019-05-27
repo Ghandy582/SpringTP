33b2a281da6913cc37d02e9a2fa15f36d633e1f8
Shusii <36765198+Shusii@users.noreply.github.com>
2019-05-27 09:53:24 +0200

    e-utjtèjtèj

--- SpringTP/src/fr/cs2i/MainApp.java
+++ SpringTP/src/fr/cs2i/MainApp.java
@@ -1,5 +1,7 @@
 package fr.cs2i;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -59,5 +61,32 @@ public class MainApp {
          (CustomEventPublisher) context.getBean("customEventPublisher");
       
       cvp.publish();  
+      
+      /*----------TEST JDBC ---------------*/
+      StudentJDBCTemplate studentJDBCTemplate = 
+    	         (StudentJDBCTemplate)context.getBean("studentJDBCTemplate");
+    	      
+    	      System.out.println("------Records Creation--------" );
+    	      studentJDBCTemplate.create("Zara", 11);
+    	      studentJDBCTemplate.create("Nuha", 2);
+    	      studentJDBCTemplate.create("Ayan", 15);
+
+    	      System.out.println("------Listing Multiple Records--------" );
+    	      List<Student> students = studentJDBCTemplate.listStudents();
+    	      
+    	      for (Student record : students) {
+    	         System.out.print("ID : " + record.getId() );
+    	         System.out.print(", Name : " + record.getName() );
+    	         System.out.println(", Age : " + record.getAge());
+    	      }
+
+    	      System.out.println("----Updating Record with ID = 2 -----" );
+    	      studentJDBCTemplate.update(2, 20);
+
+    	      System.out.println("----Listing Record with ID = 2 -----" );
+    	      Student student = studentJDBCTemplate.getStudent(2);
+    	      System.out.print("ID : " + student.getId() );
+    	      System.out.print(", Name : " + student.getName() );
+    	      System.out.println(", Age : " + student.getAge());
    }
 }
